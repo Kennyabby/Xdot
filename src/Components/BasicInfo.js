3ac8a6b4fdb7eb09dd8cd16e59ef56ed27a6073a
kennyabby <kennyabby22@gmail.com>
2022-09-07 08:29:35 +0100

    'validating--view'

--- src/Components/BasicInfo.js
+++ src/Components/BasicInfo.js
@@ -184,6 +184,7 @@ const BasicInfo = ({ getCoverList, getCoverPos, setBasicConfirmed }) => {
           infoRef.current.style.borderBottom = 'solid blue 1px'
           infoRef.current.parentElement.childNodes[1].style.display = 'block'
           infoRef.current.placeholder = ''
+          infoRef.current.scrollIntoView()
           if (infoRef.current.required) {
             infoRef.current.parentElement.childNodes[1].innerHTML = `* ${infoRef.current.title}`
           } else {
